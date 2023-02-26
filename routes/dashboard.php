ae7307f6f466eae687c13e46f46f89a059353bc0
Mostafa <mostafazarea69@gmail.com>
2023-02-26 16:16:59 +0200

    Locale

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -25,24 +25,26 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "dashboard" middleware group. Make something great!
 |
-*/
+ */
 
-Route::group(['prefix' => 'dashboard', 'as' => 'admin.'], function () {
-    Route::any('/', [HomeController::class, 'home'])->name('home');
-    Route::resources([
-        'profile' => ProfileController::class,
-        'admins' => AdminsController::class,
-        'drivers' => DriversController::class,
-        'clients' => ClientsController::class,
-        'client.addresses' => AddressController::class,
-        'type.settings' => SettingController::class,
-        'countries' => CountryController::class,
-        'country.regions' => RegionController::class,
-        'roles' => RoleController::class,
-        'permissions' => PermissionController::class,
-        'contacts' => ContactController::class,
-        'coupons' => CouponController::class,
-        'faq' => FAQController::class,
+Route::group(['middleware' => [Localization::class, 'role:Admin|Owner']], function () {
+    Route::group(['prefix' => 'dashboard', 'as' => 'admin.'], function () {
+        Route::any('/', [HomeController::class, 'home'])->name('home');
+        Route::resources([
+            'profile' => ProfileController::class,
+            'admins' => AdminsController::class,
+            'drivers' => DriversController::class,
+            'clients' => ClientsController::class,
+            'client.addresses' => AddressController::class,
+            'type.settings' => SettingController::class,
+            'countries' => CountryController::class,
+            'country.regions' => RegionController::class,
+            'roles' => RoleController::class,
+            'permissions' => PermissionController::class,
+            'contacts' => ContactController::class,
+            'coupons' => CouponController::class,
+            'faq' => FAQController::class,
 
-    ]);
+        ]);
+    });
 });
