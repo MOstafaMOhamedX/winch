5c12baa3e05cb358eba1cd3ca6e62a1585c22786
Mostafa <mostafazarea69@gmail.com>
2023-02-26 16:06:35 +0200

    APP Name

--- resources/views/dashboard/layout.blade.php
+++ resources/views/dashboard/layout.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>{{ setting('title_'.lang()) }}</title>
+    <title>{{ setting('title_'.lang()) ?? env('APP_NAME', 'EMCAN') }}</title>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
